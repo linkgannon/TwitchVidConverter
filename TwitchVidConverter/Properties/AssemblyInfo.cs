a916b874a09ac44fcf84c4b23916b2e504234413
linkgannon <cgoodwi8@gmail.com>
2022-07-18 23:03:54 -0400

    Updated assembly info and added an app icon.

--- TwitchVidConverter/Properties/AssemblyInfo.cs
+++ TwitchVidConverter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TwitchVidConverter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Convert Twith downloaded clip and highlight file names to use the title.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("linkgannon")]
 [assembly: AssemblyProduct("Twitch Vid Converter")]
